6047a40602908aa610311b4678d301f4fd884df8
ctweehaur <ctweehaur@gmail.com>
2026-06-02 18:19:24 +0800

    Create shiwuliangmao_translation.js

---
+++ shiwuliangmao_translation.js
@@ -0,0 +1,17 @@
+const translationData = [
+    {
+        zh: "某个人非常讨厌老鼠，他不惜倾尽家财去寻求一只捕鼠能手。买来猫后，主人天天用鲜鱼肥肉喂饱它，还用温暖舒适的毛毯让它睡觉。",
+        py: "mǒu gè rén fēi cháng wù shǔ tā bù xī pò jiā qù qiú qǔ yī zhī liáng māo mǎi lái māo hòu zhǔ rén tiān tiān yòng xīng gāo wèi bǎo tā hái yòng zhān jì ràng tā mián shuì",
+        en: "A certain man detested rats so much that he spent his entire family fortune to acquire an excellent mouser. Once he got the cat, he fed it choice meat and fish daily, and let it sleep on cozy, warm wool carpets."
+    },
+    {
+        zh: "这只猫既然吃得饱饱的而且生活极其安逸，往往根本就不去捕捉老鼠。更严重的是，它竟然还和老鼠们一起玩耍嬉戏，老鼠们因为这个缘故反而更加猖獗肆虐。",
+        py: "zhè zhī māo jì rán chī dé bǎo bǎo de ér qiě shēng huó jí qí ān yì shuài bèn gēn běn bú qù bǔ shǔ shèn zhě tā jìng rán hái yǔ shǔ yóu xì shǔ mǐn yǐ gù fǎn ér yì bào",
+        en: "Since the cat was constantly well-fed and lived in absolute luxury, it generally refused to catch any rats at all. Worse still, it even began to play and frolic with the rats, and because of this, the rats became even more rampant and destructive."
+    },
+    {
+        zh: "主人非常愤怒，于是再也不养猫了，并且主观地认为天底下根本就没有真正会捕鼠的好猫。",
+        py: "zhǔ rén fēi cháng fèn nù yú shì sù bú fù xù māo le bìng qiě yǐ wéi tiān xià gēn běn jiù méi yǒu zhēn zhèng huì bǔ shǔ de liáng māo",
+        en: "The man grew completely furious, stopped keeping cats altogether, and concluded that there were simply no good mousing cats left in the world."
+    }
+];
